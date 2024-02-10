88f82e01366b395464e92acd34041e1921b61f48
cvennevik <cvennevik@gmail.com>
2024-02-10 20:05:32 +0100

    Migrate from Hugo to 11ty

---
+++ .eleventy.js
@@ -0,0 +1,39 @@
+const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require("luxon");
+
+const md = markdownIt({ html: true }).use(markdownItAnchor, { tabIndex: false });
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addWatchTarget("src/css/");
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: "<!--more-->",
+  });
+  eleventyConfig.setLibrary('md', md);
+
+  /*** FILTERS ***/
+  eleventyConfig.addFilter("absoluteUrl", (url, base) => {
+    return new URL(url, base).toString();
+  });
+  eleventyConfig.addFilter("dateToRfc2822", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toRFC2822();
+  });
+  eleventyConfig.addFilter("renderFullDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj)
+      .setLocale("en-US")
+      .toLocaleString(DateTime.DATE_FULL);
+  });
+  eleventyConfig.addFilter("renderPostSummary", (post) => {
+    return md.render(post.data.summary ?? post.page.excerpt);
+  });
+
+  /*** RETURN ***/
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};

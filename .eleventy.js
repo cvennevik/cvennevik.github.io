7077c4319c6cdbb307620702dd8fe1cd3d1512ad
cvennevik <cvennevik@gmail.com>
2024-05-20 15:02:33 +0200

    Replace renderFullDate filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const markdownIt = require("markdown-it");
-const { DateTime } = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img/");
@@ -10,11 +9,6 @@ module.exports = function (eleventyConfig) {
   /*** FILTERS ***/
   eleventyConfig.addFilter("absoluteUrl", (url, base) => {
     return new URL(url, base).toString();
-  });
-  eleventyConfig.addFilter("renderFullDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj)
-      .setLocale("en-US")
-      .toLocaleString(DateTime.DATE_FULL);
   });
   eleventyConfig.addFilter("renderBlogPostSummary", (post) => {
     return markdownIt({ html: true }).render(post.data.summary ?? post.page.excerpt ?? "");

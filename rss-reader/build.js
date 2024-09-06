809917dc33690342984dd3b2773348d3224301a2
cvennevik <cvennevik@gmail.com>
2024-09-06 18:31:28 +0200

    rss-reader: Combine files into one

--- rss-reader/build.js
+++ rss-reader/build.js
@@ -1,14 +1,33 @@
 /**
- * 🦉 Bubo RSS Reader
- * ====
- * Dead, dead simple feed reader that renders an HTML
- * page with links to content from feeds organized by site
+ * Code based on Bubo RSS Reader, via kevinfiol, with the following license:
+ * 
+ * ===
+ * MIT License
+ * 
+ * Copyright (c) 2021 George Mandis, kevinfiol
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * ===
  *
  */
 
 import Parser from 'rss-parser';
 import { writeFileSync } from 'node:fs';
-import { template } from './template.js';
 
 
 /* Config */
@@ -157,6 +176,72 @@ function escapeHtml(html) {
     .replaceAll('"', '&quot;');
 }
 
+/* Template */
+function forEach(arr, fn) {
+  let str = '';
+  arr.forEach(i => str += fn(i) || '');
+  return str;
+}
+
+function template({ groups, errors, now }) {
+  return (`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cecilie's Feed Reader</title>
+  <style>
+    summary { cursor: pointer; }
+    summary:hover { opacity: .75; }
+    .feed-url { color: #aaa; }
+  </style>
+</head>
+<body>
+  <h1>Cecilie's Feed Reader</h1>
+  <p>
+    Last updated ${now.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric' })}.
+  </p>
+
+  <main>
+    ${forEach(groups, ([groupName, feeds]) => `
+      <h2>${groupName}</h2>
+
+      ${forEach(feeds, feed => `
+        <details>
+          <summary>
+            ${feed.title}
+            <span class="feed-url">(${feed.feed})</span>
+          </summary>
+          <ul>
+            ${forEach(feed.items, item => `
+              <li>${item.timestamp}: <a href="${item.link}">${item.title}</a></li>
+            `)}
+          </ul>
+        </details>
+      `)}
+    `)}
+  </main>
+
+  <footer>
+    ${errors.length > 0 ? `
+      <h2>Errors</h2>
+      <p>There were errors trying to parse these feeds:</p>
+      <ul>
+      ${forEach(errors, error => `
+        <li>${error}</li>
+      `)}
+      </ul>
+    ` : ''}
+
+    <p>
+      Powered by <a href="https://github.com/kevinfiol/rss-reader">Bubo Reader</a>, a project by <a href="https://george.mand.is">George Mandis</a> and <a href="https://kevinfiol.com">Kevin Fiol</a>.
+    </p>
+  </footer>
+</body>
+</html>
+`);
+}
 
 /* Build */
 const parser = new Parser();

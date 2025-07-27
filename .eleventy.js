36f805be6adb849226a05daa56976b81759af432
cvennevik <cvennevik@gmail.com>
2025-07-27 18:17:23 +0200

    Store site-wide CSS in separate /css/site.css file

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img/");
   eleventyConfig.addPassthroughCopy("src/font/");
+  eleventyConfig.addPassthroughCopy("src/css/")
   eleventyConfig.addWatchTarget("src/img/");
 
   /*** FILTERS ***/
